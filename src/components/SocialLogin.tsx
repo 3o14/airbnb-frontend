a1bdb452055aa20ffa7542194911a03487dead4e
3o14 <dldnjswn4265@gmail.com>
2023-01-28 18:24:31 +0900

    일반 로그인을 위한 useState

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -2,12 +2,26 @@ import { FaComment, FaGithub } from "react-icons/fa";
 import { Box, Button, Divider, HStack, Text, VStack } from "@chakra-ui/react";
 
 export default function SocialLogin() {
-  const kakaoParams = {
+  let kakaoParams = {
     client_id: "99624fd3a7cf3d03da77b4d288d81241",
     redirect_uri: "http://127.0.0.1:3000/social/kakao",
     response_type: "code",
   };
-  const params = new URLSearchParams(kakaoParams).toString();
+  const githubParams = {
+    client_id: "5195598d392601f20eea",
+    scope: "read:user,user:email",
+  };
+
+  // 깃헙, 카카오 각 소셜 로그인 버튼의 url
+  const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?${new URLSearchParams(
+    kakaoParams
+    ).toString()}`;
+  const githubUrl = `https://github.com/login/oauth/authorize?${new URLSearchParams(
+    githubParams
+  ).toString()}`;
+      // url : [github/kakao 문서에서 제공해준 host url] ? [client ID] & [scope]
+      // scope란, 사용자로부터 얻고 싶은 정보를 말함 (scope 설정이 없으면 public 정보만 받을 수 있음)
+
   return (
     <Box mb={4}>
       <HStack my={8}>
@@ -21,9 +35,8 @@ export default function SocialLogin() {
         {/* 깃헙 로그인 */}
         <Button 
           as="a" // <a></a>(anchor)로서 사용하겠다는 의미
-          href="https://github.com/login/oauth/authorize?client_id=768dec930d2aa450d693&scope=read:user,user:email" // a태그 url
-          // href url : [github 문서에서 제공해준 url] ? [client ID] & [scope]
-          // scope란, 사용자로부터 얻고 싶은 정보를 말함 (scope 설정이 없으면 public 정보만 받을 수 있음)
+          href={githubUrl} // a태그 url
+          
           w="100%"
           leftIcon={<FaGithub />}
         >
@@ -32,7 +45,7 @@ export default function SocialLogin() {
         {/* 카카오 로그인 */}
         <Button
           as="a"
-          href={`https://kauth.kakao.com/oauth/authorize?${params}`}
+          href={kakaoUrl}
           w="100%"
           leftIcon={<FaComment />}
           colorScheme={"yellow"}
--- src/lib/useUser.ts
+++ src/lib/useUser.ts
@@ -5,6 +5,7 @@ import { IUser } from "../types";
 export default function useUser() {
     const { isLoading, data, isError } = useQuery<IUser>(["me"], getMe, {
     retry: false, // 자동으로 재시도를 반복하는 reactQuery에게 한 번만 시도해도 된다는 것을 알려줌 
+    refetchOnWindowFocus: false,
   });
   return {
     userLoading: isLoading,
